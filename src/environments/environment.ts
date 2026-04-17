5cbb2436c805d5f8d69c3e9e2235e454f1572b90
Aleja <ddagmaaralejandra@gmail.com>
2026-04-16 21:27:47 -0500

    no backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8081/api'
+// };
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8081/api'
+  production: true,
+  apiUrl: ''
 };
