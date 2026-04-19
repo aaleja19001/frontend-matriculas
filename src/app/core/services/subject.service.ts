65e2d9d8d81c482ddbb719913b29e182d5bf0e40
Aleja <ddagmaaralejandra@gmail.com>
2026-04-18 23:36:55 -0500

    feat: agregar materias y programas en el front

--- src/app/core/services/subject.service.ts
+++ src/app/core/services/subject.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
 
 export interface Subject {
   id?: number;
-  name?: string;
-  code?: string;
-  credits?: number;
+  name: string;
+  code: string;
+  credits: number;
+  programs?: any[];
 }
 
 @Injectable({ providedIn: 'root' })
@@ -15,7 +17,23 @@ export class SubjectService {
 
   constructor(private http: HttpClient) {}
 
-  getAll() {
+  getAll(): Observable<Subject[]> {
     return this.http.get<Subject[]>(this.url);
+  }
+
+  getById(id: number): Observable<Subject> {
+    return this.http.get<Subject>(`${this.url}/${id}`);
+  }
+
+  create(subject: Subject): Observable<Subject> {
+    return this.http.post<Subject>(this.url, subject);
+  }
+
+  update(id: number, subject: Subject): Observable<Subject> {
+    return this.http.put<Subject>(`${this.url}/${id}`, subject);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`);
   }
 }
